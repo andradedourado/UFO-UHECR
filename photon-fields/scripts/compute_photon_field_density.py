Fix computation of the differential number density of photons

photon-fields/scripts/compute_photon_field_density.py
@@ -6,7 +6,7 @@ erg_to_eV = 6.241509074e11
 solar_mass_to_g = 1.989e33
 
 c = 2.99792458e10 # cm / s
-G = 6.67430e-8 # m^3 / g / s^2
+G = 6.67430e-8 # cm^3 / g / s^2
 h = 6.62607015e-27  # erg * s
 
 # Benchmark model 
@@ -37,7 +37,7 @@ def write_photon_field_density(region): # [Number of photons per unit volume and
     data = np.loadtxt(f"{RESULTS_DIR}/photon_field_luminosity_{region}.dat")
         
     E = h * data[:,0] # erg
-    nu_L_nu = data[:,0] * data[:,1]
+    nu_L_nu = data[:,0] * data[:,1] # erg / s
     photon_number_density = nu_L_nu / (4 * np.pi * region_radius(region)**2 * c * E**2 * erg_to_eV)
         
     np.savetxt(f"{RESULTS_DIR}/photon_field_density_{region}.dat", np.column_stack((E * erg_to_eV, photon_number_density)), fmt = "%.15e")

photon-fields/scripts/compute_photon_field_luminosity_disk.py
@@ -8,7 +8,7 @@ eV_to_erg = 1.602176634e-12
 solar_mass_to_g = 1.989e33
 
 c = 2.99792458e10 # cm / s
-G = 6.67430e-8 # m^3 / g / s^2
+G = 6.67430e-8 # cm^3 / g / s^2
 h = 6.62607015e-27 # erg * s
 k = 1.380649e-16 # erg / K
 sigma = 5.670374419e-5 # erg / s / cm^2 / K^4
@@ -59,7 +59,7 @@ def compute_photon_field(nu_array):
 # ----------------------------------------------------------------------------------------------------
 def write_photon_field():
 
-    nu_array = np.logspace(13, 17, num = 100) # Hz    
+    nu_array = np.logspace(11, 17, num = 100) # Hz    
     np.savetxt(f"{RESULTS_DIR}/photon_field_luminosity_disk.dat", np.column_stack((nu_array, compute_photon_field(nu_array))), fmt = "%.15e")
 
 # ----------------------------------------------------------------------------------------------------

photon-fields/scripts/compute_photon_field_luminosity_torus.py
@@ -30,7 +30,7 @@ def compute_photon_field(nu):
 # ----------------------------------------------------------------------------------------------------
 def write_photon_field():
 
-    nu = np.logspace(11, 14, num = 100) # Hz
+    nu = np.logspace(9, 14, num = 100) # Hz
     np.savetxt(f"{RESULTS_DIR}/photon_field_luminosity_torus.dat", np.column_stack((nu, compute_photon_field(nu))), fmt = "%.15e")
 
 # ----------------------------------------------------------------------------------------------------